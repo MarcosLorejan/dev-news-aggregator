a14e5e8225217a98bca93f892425728f729594d8
marcos-lorejan <marcos.lorejan@gmail.com>
2025-09-10 16:07:41 -0300

    fix: remove trailing whitespace from ReadArticlesController

--- app/controllers/read_articles_controller.rb
+++ app/controllers/read_articles_controller.rb
@@ -9,7 +9,7 @@ class ReadArticlesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @article.mark_as_read!
-    
+
     respond_to do |format|
       format.html { redirect_to articles_path, notice: "Article marked as read" }
       format.json { render json: { message: "Article marked as read", read: true }, status: :ok }
@@ -23,7 +23,7 @@ class ReadArticlesController < ApplicationController
 
   def destroy
     @article = Article.find(params[:article_id])
-    
+
     unless @article.read?
       respond_to do |format|
         format.html { redirect_to read_articles_path, alert: "Article is not marked as read" }
@@ -31,9 +31,9 @@ class ReadArticlesController < ApplicationController
       end
       return
     end
-    
+
     @article.unmark_as_read!
-    
+
     respond_to do |format|
       format.html { redirect_to read_articles_path, notice: "Article marked as unread" }
       format.json { render json: { message: "Article marked as unread", read: false }, status: :ok }
