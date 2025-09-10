a74afa9827f860cf6ecd409534b38352ac4e962d
marcos-lorejan <marcos.lorejan@gmail.com>
2025-09-10 12:31:18 -0300

    feat: add safe_external_url helper for URL validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def safe_external_url(url)
+    return nil if url.blank?
+    
+    begin
+      uri = URI.parse(url)
+      return url if uri.scheme&.match?(/\Ahttps?\z/) && uri.host.present?
+    rescue URI::InvalidURIError
+      # Invalid URI format
+    end
+    
+    nil
+  end
 end
