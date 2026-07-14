00ea1cd53d50d5d543fff7f5df7f36407f37a166
Amayyas <amayyas.aouadene@epitech.eu>
2026-07-14 18:01:07 +0200

    docs: align database credentials across env files

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
--- Create test database
+-- Create test database.
+-- The Postgres entrypoint runs this as POSTGRES_USER (see docker-compose.yml),
+-- so that role owns the database and needs no extra grant. Keeping the owner
+-- implicit means changing POSTGRES_USER in .env does not break this script.
 CREATE DATABASE dev_news_aggregator_test;
-GRANT ALL PRIVILEGES ON DATABASE dev_news_aggregator_test TO dev_news_user;
